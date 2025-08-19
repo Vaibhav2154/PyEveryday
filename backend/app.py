aa25316221688c36b7f6f26fcb2ff90a2ff527e8
Keshav Maheshwari <Keshav1534@gmail.com>
2025-08-19 12:54:07 +0530

    Added basic files for auth

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from .routers.auth.router import auth_router
+version = "v1.0"
 
-app = FastAPI()
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print("Intializing API server...")
+    ## This function allows the fastapi server to initiate the resouces which server is going to use
+    ## Like initiating the database here
+    yield
+    
+    # Here add the resources, script which server needs to perform before exiting the server.
+    print("Exiting the API server...")
+    
+app = FastAPI(
+    title= "PyEveryday",
+    description= "Pyeveryday API for handeling the backend.",
+    version=version,
+    lifespan= life_span
+)
 
-@app.get("/check")
-async def check():
-    return {"message": "We are checking the API"}
+
+### Add midelware here...
+
+app.include_router(auth_router.router, prefix=f"/api/{version}/auth", tags=['auth'])
